33d43e168ffd0435b7daf5439647e42ef386f2eb
Mikhail <MikhailMeshcheryakov@effits.ru>
2015-02-17 19:40:38 +0300

    Update declare.py
    Python3 code for insert data into SQLite Table

--- declare.py
+++ declare.py
@@ -3,6 +3,9 @@
 import sqlite3;
 import os;
 
+сonn = sqlite3.connect('/tmp/leftpart')
+c = conn.cursor()
+
 #insert NUM info to table
 disp = os.environ["$NUM"]
 cur.execute("INSERT INTO leftpart(Display) VALUES %s" % disp)
@@ -10,3 +13,5 @@ cur.execute("INSERT INTO leftpart(Display) VALUES %s" % disp)
 # insert USER info to table
 usr = os.environ["$USER"]
 cur.execute("INSERT INTO leftpart(USER) VALUES %s" % usr)
+
+conn.close()
